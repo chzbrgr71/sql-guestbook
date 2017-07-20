cb2a73a0bf910b4472ab670042c939454c249e26
Brian Redmond <brianisrunning@gmail.com>
2017-07-20 08:05:08 -0700

    fixed go app

--- web_app_go/sqlguestbook.go
+++ web_app_go/sqlguestbook.go
@@ -25,9 +25,8 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 	var htmlHeader = "<!DOCTYPE html><html><head><style>table {font-family: arial, sans-serif;border-collapse: collapse;width: 100%;}td, th {border: 1px solid #dddddd;text-align: left;padding: 8px;}tr:nth-child(even) {background-color: #dddddd;}</style></head><body>"
 	fmt.Fprintf(w, htmlHeader)
 	var hostname = getHostname()
-	var gitSHA = os.Getenv("GIT_SHA")
-	var appversion = "1.9.5"
-	fmt.Fprintf(w, "<h1>Msft Ready Golang Guestbook (v%s)</h1><p>Hostname: %s</p><p>Git: %s</p><table><tr><th>Date</th><th>Name</th><th>Phone</th><th>Sentiment</th><th>Message</th></tr>", appversion, hostname, gitSHA)
+	var appversion = "2.0"
+	fmt.Fprintf(w, "<h1>Golang Guestbook (v%s)</h1><p>Hostname: %s</p><table><tr><th>Date</th><th>Name</th><th>Phone</th><th>Sentiment</th><th>Message</th></tr>", appversion, hostname)
 
 	// query DB and loop through rows
 	var connString = getConnectString()
